7f15b27e80a3431abfca459588b9d79b51537a13
Luto228 <megapupsik909@gmail.com>
2026-05-03 11:52:57 +0400

    Add dynamic terminal detection

--- main.sh
+++ main.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 ACTIVE=$1
-
+CHOICE_TERMINAL=("gnome-terminal" "kitty" "konsole" "xfce4-terminal" "terminator" "urxvt")
+for i in "${CHOICE_TERMINAL[@]}"; do
+    I_TERMINAL=$(command -v $i)
+    if [ -z "$I_TERMINAL" ]; then
+        continue
+    else
+        CURRENT_TERMINAL=$i
+        break
+    fi
+done
+if [ -z "$CURRENT_TERMINAL" ]; then
+    echo "No supported terminal found"
+    exit 1
+fi
 if [ "$ACTIVE" == "true" ]; then
-    gnome-terminal
+    $CURRENT_TERMINAL &
     exit 0
-else
-    WINDOW_ID=$(wmctrl -l | grep -i "Terminal" | head -n 1 | awk '{print $1}')
+elif [ "$ACTIVE" != "true" ]; then
+    WINDOW_ID=$(wmctrl -lx | grep -i "$CURRENT_TERMINAL" | head -n 1 | awk '{print $1}')
     if [ -z "$WINDOW_ID" ]; then
-        gnome-terminal
+        $CURRENT_TERMINAL &
     else
         wmctrl -i -a "$WINDOW_ID"
     fi
 fi
-
